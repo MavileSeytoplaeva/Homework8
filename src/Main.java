f06cab83ddae2e0fa17e4d589ae6bd3e2164298e
MavileSeytoplaeva <mavile.seitoplaeva@gmail.com>
2023-03-09 16:30:20 +0300

    Домашка сделана 1 часть массивы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,106 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int[] numbers = {1, 2, 3};
+        float[] fractionalNumbers = {1.57f, 7.654f, 9.986f};
+        int[] bigNumbers = {45_768, 768_769, 6_768_745};
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        //Распечатайте на отдельной строчке элементы каждого массива по порядку через запятую. В конце строки запятую ставить не надо.
+        int[] numbers = {1, 2, 3};
+        float[] fractionalNumbers = {1.57f, 7.654f, 9.986f};
+        int[] bigNumbers = {45_768, 768_769, 6_768_745};
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(fractionalNumbers));
+        System.out.println(Arrays.toString(bigNumbers));
+//        for (int i = 0; i < 3; i++) {
+//            if (i == 2) {
+//                System.out.print(numbers[i]);
+//                break;
+//            }
+//            System.out.print(numbers[i]+", ");
+//        }
+//        System.out.println();
+//        for (int i = 0; i < 3; i++) {
+//            if (i == 2) {
+//                System.out.print(fractionalNumbers[i]);
+//                break;
+//            }
+//            System.out.print(fractionalNumbers[i]+", ");
+//        }
+//        System.out.println();
+//        for (int i = 0; i < 3; i++) {
+//            if (i == 2) {
+//                System.out.print(bigNumbers[i]);
+//                break;
+//            }
+//            System.out.print(bigNumbers[i]+", ");
+//        }
+//        System.out.println();
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        //Распечатайте на отдельной строчке элементы каждого массива в обратном порядке через запятую. В конце строки запятую ставить не надо.
+        int[] numbers = {1, 2, 3};
+        float[] fractionalNumbers = {1.57f, 7.654f, 9.986f};
+        int[] bigNumbers = {45_768, 768_769, 6_768_745};
+        for (int i = 2; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(numbers[i]);
+                break;
+            }
+            System.out.print(numbers[i]+", ");
+        }
+        System.out.println();
+        for (int i = 2; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(fractionalNumbers[i]);
+                break;
+            }
+            System.out.print(fractionalNumbers[i]+", ");
+        }
+        System.out.println();
+        for (int i = 2; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(bigNumbers[i]);
+                break;
+            }
+            System.out.print(bigNumbers[i]+", ");
+        }
+        System.out.println();
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        //Пройдитесь по первому целочисленному массиву и все нечетные числа в нем сделайте четными (нужно прибавить 1).
+        int[] numbers = {1, 2, 3};
+        int[] bigNumbers = {45_768, 768_769, 6_768_745};
+        for (int i = 0; i < 3; i++) {
+            if (i % 2 == 0){
+                System.out.println(numbers[i]+1);
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (i % 2 == 0){
+                System.out.println(bigNumbers[i]+1);
+                break;
+            }
+        }
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+        //
     }
 }
+
